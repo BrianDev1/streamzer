30c36c072811ebaed73d506e1b66db5891ba56d6
Brian Rudolph <brian.rudolph303@gmail.com>
2021-04-18 11:01:09 -0600

    fix: Updated types and added appbar to route

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,35 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/streamCreate";
+import StreamDelete from "./streams/streamDelete";
+import StreamEdit from "./streams/streamEdit";
+import StreamList from "./streams/streamList";
+import StreamShow from "./streams/streamShow";
+import StreamzerAppBar from "./streamzerAppBar";
 
 const App = () => {
-
-    return (
-        <div><BrowserRouter>
+  return (
+    <div>
+      <StreamzerAppBar />
+      <BrowserRouter>
         <div>
-            <Route path="/stream-delete" />
-            <Route path="/stream-show" />
-            <Route path="/stream-edit" />
-            <Route path="/stream-create"/>
-            <Route path="/stream-list" />
+          <Route
+            exact
+            path="/streams/delete"
+            component={() => <StreamDelete />}
+          />
+          <Route exact path="/streams/edit" component={() => <StreamEdit />} />
+          <Route exact path="/streams/show" component={() => <StreamShow />} />
+          <Route
+            exact
+            path="/streams/create"
+            component={() => <StreamCreate />}
+          />
+          <Route exact path="/" component={() => <StreamList />} />
         </div>
-        </BrowserRouter></div>
-    );
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/streamzerAppBar.tsx
@@ -0,0 +1,8 @@
+import AppBar from "@material-ui/core/AppBar";
+import React from "react";
+
+const StreamzerAppBar = () => {
+  return <AppBar>Hello</AppBar>;
+};
+
+export default StreamzerAppBar;
