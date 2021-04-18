219be549fdf644f33eb85d2674a575cff0cb5189
Brian Rudolph <brian.rudolph303@gmail.com>
2021-04-18 10:38:03 -0600

    Updated types and addes routes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
 
-
     return (
-        <div>App</div>
+        <div><BrowserRouter>
+        <div>
+            <Route path="/stream-delete" />
+            <Route path="/stream-show" />
+            <Route path="/stream-edit" />
+            <Route path="/stream-create"/>
+            <Route path="/stream-list" />
+        </div>
+        </BrowserRouter></div>
     );
 }
 
---
+++ src/components/streams/streamDelete.tsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const StreamDelete = () => {
+    return (<div>StreamShow</div>);
+}
+
+export default StreamDelete;
